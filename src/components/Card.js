1792fa3d100b54ecba28a0d6e421ceeb3cf7096d
lalaphalla <lalaphalla@gmail.com>
2023-11-26 00:56:01 +0700

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+export default function Card() {
+  const percentage = 66;
+
+  return (
+    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 relative">
+      <a href="#">
+        <img
+          class="rounded-t-lg"
+          src="https://www.themoviedb.org/t/p/w220_and_h330_face/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg"
+          alt=""
+        />
+      </a>
+      <div class="p-5">
+        <a href="#">
+          <div class="w-10 h-10 bg-gray-200 rounded-full dark:bg-gray-700 absolute top-[310px] left-4">
+            <CircularProgressbar value={percentage} text={`${percentage}%`} />
+          </div>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Oppenheimer
+        </p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Jul 19, 2023
+        </p>
+      </div>
+    </div>
+  );
+}
